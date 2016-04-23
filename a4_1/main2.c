0087d36b250b8e6256539ace6704ebee8d785089
Joseph Long <jcl@kissntellstore.com>
2016-04-22 19:54:39 -0700

    added main2.c

---
+++ a4_1/main2.c
@@ -0,0 +1,19 @@
+/*
+ * helloWorld.c
+ *
+ *  Created on: Apr 22, 2016
+ *      Author:fadda admin
+ */
+
+/* Hello dWorld git edited */
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello Git....");
+    printf("test 1..ddd.\n");
+    return 0;
+
+
+}
